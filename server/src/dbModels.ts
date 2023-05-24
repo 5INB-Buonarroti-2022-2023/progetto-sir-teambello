b9a4a2dc658a29c34451014bee2110a760b5a1e0
tommasobazzanella <tommaso.bazzanella@buonarroti.tn.it>
2023-05-24 23:39:48 +0200

    new dbModels modified main

---
+++ server/src/dbModels.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const imageSchema = new mongoose.Schema({
+    id: {type: Number, required: true},
+    path: {type: String, required: true},
+    result: {type: Number, required: true},//in teoria json non e un tipo ammesso ma non segnala errori
+});
+
+export const imageModel = mongoose.model("image", imageSchema);
+//export default imageModel;
+
+const userSchema = new mongoose.Schema({
+    userName: {type: String, required: true},
+    hashPassword: {type: String, required: true},
+    mail: {type: String, required: true}, //forse
+});
+
+export const userModel = mongoose.model("user", userSchema);
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,22 +4,31 @@ import cors from "cors";
 import mongoose from "mongoose";
 import path from "path";
 import { app } from "./server";
-
+import image from './dbModels';
 
 //db connection to app db
-/*  Usate questa sezione se volete usare un db mongodb
+//  Usate questa sezione se volete usare un db mongodb
 
-const database = "test"
-const uri = ``;
+/*const database = "test"
+const uri = `mongodb+srv://Bacteria:Bacteria@bacteria.vjhddyx.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.set('strictQuery', false);
 mongoose.connect(uri, async () => {
 
-});
-*/
+});*/
+mongoose.connect("mongodb+srv://Bacteria:Bacteria@bacteria.vjhddyx.mongodb.net/?retryWrites=true&w=majority")
+
 
 const PORT = 3000;
 
 app.listen(PORT, ()  => {
     console.log("hi! server started")
 });
+
+const immagine = new image({
+    id: 1,
+    path: "/ciao",
+    result: 10,
+});
+
+await immagine.save();
 
